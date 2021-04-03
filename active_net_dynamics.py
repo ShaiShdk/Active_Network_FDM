d9afc9932f4775006a083c599ab175ac190eb879
ShaiShad <47192449+ShaiShdk@users.noreply.github.com>
2021-04-03 03:32:44 -0700

    Update active_net_dynamics.py

--- active_net_dynamics.py
+++ active_net_dynamics.py
@@ -656,25 +656,3 @@ class random_network:
             endAvg  = np.abs(self.ed_ver_sps)
             edcents = endAvg.dot(self.verts)
             return edcents
-
-        # def coarse_grain(self):
-        #
-        #     import matplotlib.pyplot as plt
-        #     import numpy as np
-        #
-        #     methods = [None, 'none', 'nearest', 'bilinear', 'bicubic', 'spline16',
-        #                'spline36', 'hanning', 'hamming', 'hermite', 'kaiser', 'quadric',
-        #                'catrom', 'gaussian', 'bessel', 'mitchell', 'sinc', 'lanczos']
-        #
-        #     # Fixing random state for reproducibility
-        #     np.random.seed(19680801)
-        #     grid = np.random.rand(4, 4)
-        #     fig, axs = plt.subplots(nrows=3, ncols=6, figsize=(9, 6),
-        #                             subplot_kw={'xticks': [], 'yticks': []})
-        #
-        #     for ax, interp_method in zip(axs.flat, methods):
-        #         ax.imshow(grid, interpolation=interp_method, cmap='viridis')
-        #         ax.set_title(str(interp_method))
-        #
-        #     plt.tight_layout()
-        #     plt.show()
