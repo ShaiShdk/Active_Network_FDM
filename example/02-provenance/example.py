0ff6b8a79606724e71e346290e901aabd0f91697
Dominik Schildknecht <dominik.schildknecht@gmail.com>
2021-07-14 16:04:26 -0700

    Add option to work with viscous_dynamics

--- example/02-provenance/example.py
+++ example/02-provenance/example.py
@@ -15,14 +15,14 @@ ex.dependencies.add(sacred.dependencies.PackageDependency("ANFDM", ANFDM_VERSION
 @ex.config
 def cfg():
     # Geometry parameters
-    UnitCell_Geo        = 'Square'#{'Square''Hexagon''Triangular''Random'}
-    lattice_shape       = [50, 50]
-    Global_Geometry     = 'Rectangle'#{'Rectangle'"Triangle""Hexagon""Hexagram""Circle""Ellipse"}
+    UnitCell_Geo        = 'Triangular'#{'Square''Hexagon''Triangular''Random'}
+    lattice_shape       = [50,50]
+    Global_Geometry     = 'Hexagon'#{'Rectangle'"Triangle""Hexagon""Hexagram""Circle""Ellipse"}
     lattice_dsrdr       = 0
     rounded             = 'False'
     round_coeff         = 2
     AR_xy               = [1, 0.5]
-    AR_fr               = 1
+    AR_fr               = 1.
     illum_ratio         = 1
     rhoPower            = 1
     plot_full_positions = True
@@ -35,10 +35,11 @@ def cfg():
     rhof , rhoi = 1 , 0.5
     s0 , gamma  = 0.2 , 0.0
 
-    T_tot , dt  = 100 , 0.1
-    N_frame     = 10
+    T_tot , dt  = 1000 , 0.1
+    N_frame     = 25
     tau_s       = 1
     mass        = 10
+    integration_type = 'viscous'
 
 
 @ex.automain
@@ -47,7 +48,9 @@ def main(_config, _run):
     net = random_network(_config, experiment = ex)
 
     # Integrate it up
-    net.Inertial_Dynamics(_config['T_tot'],
+    integration_method = _config.get('integration_method', 'inertial').lower()
+    if integration_method == 'inertial':
+        net.Inertial_Dynamics(_config['T_tot'],
                           _config['dt'],
                           _config['mass'],
                           _config['gamma'],
@@ -56,4 +59,16 @@ def main(_config, _run):
                           _config['tau_s'],
                           N_frame = _config['N_frame']
                           )
+    elif integration_method == 'viscous': 
+        net.Viscous_Dynamics(_config['T_tot'],
+                          _config['dt'],
+                          #_config['mass'],
+                          _config['gamma'],
+                          _config['rhoi'],
+                          _config['s0'],
+                          _config['tau_s'],
+                          N_frame = _config['N_frame']
+                          )
+    else:
+        raise RuntimeError(f"Don't understand integration type {integration_method}")
 
